ae5f1688615f706d73703b6749376aa173f18682
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-16 13:21:49 +0100

    moved things to functions instead of main file

--- main.c
+++ main.c
@@ -1,285 +1,284 @@
-/*
-
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <limits.h>
 #include <stdbool.h>
-#include <time.h>
-#include <string.h>
-#include <ctype.h>
 
-#define PI 3.14159265359				//definierar PI
 
-double plus(double t1, double t2)		//Funktion Plus		
-{										//Skickar tillbaka resultatet av tal1 plus tal2
-	return t1 + t2;
+
+int menu_root() {
+	int val;
+	printf("\n");
+	printf("V\x84lj vilka storheter du vill ber�kna:\n");
+	printf("V\x84lj 1 f\x94r: OHMS LAG\n");
+	printf("V\x84lj 2 f\x94r: Rtot\n");
+	printf("V\x84lj 3 f\x94r: EFFEKTLAGEN ENKEL\n");
+	printf("V\x84lj 4 f\x94r: SKENBAR EFFEKT ENFAS\n");
+	printf("V\x84lj 5 f\x94r: AKTIV EFFEKT/MEDELEFFEKT ENFAS\n");
+	printf("V\x84lj 6 f\x94r: SKENBAR EFFEKT 3-FAS\n");
+	printf("V\x84lj 7 f\x94r: AKTIV EFFEKT 3-FAS\n");
+	printf("V\x84lj 0 f\x94r: ATT AVSLUTA\n");
+	scanf("%d", &val);
+	return val;
 }
 
-double minus(double t1, double t2)		//Funktion minus		
-{										//Skickar tillbaka resulttet av tal1 minus tal2
-	return t1 - t2;
+double ohms_lag(double r, double i){
+
+    double u = i * r;
+
+    return u;
 }
 
-double ganger(double t1, double t2)		//Funktion Ganger		
-{										//Skickar tillbaka resultatet av tal1 ganger tal2		
-	return t1 * t2;
+double res_tot(double r1, double r2, double r3){
+
+
+    double rtot= (1/r1) + (1/r2) + (1/r3);
+    rtot= 1/rtot;
+
+    return rtot;
 }
 
-double delat(double t1, double t2)		//Funktion Delat		
-{										//Skickar tillbaka resultatet av tal1 delat med tal2
-	return t1 / t2;
+double eff_enk(double u, double i){
+
+    double p = u * i;
+
+    return p;
 }
 
-double t1pi(double t1)					//Funktion PI			
-{										//Skickar tillbaka resultatet av tal1 ganger PI
-	return (t1 * PI);
+double sken_eff(double u, double i){
+
+    double s = u * i;
+
+    return s;
+
 }
 
-double rotenur(double t1)				//Funktion Rotenur	
-{										//Skickar tillbaka resultatet av roten ur tal1
-	return sqrt(t1);
+double aktiv_eff(double u, double i, double cos){
+
+    double p = u * i * cos;
+
+    return p;
+
 }
 
-double omkrets_cirkel(double r)			//Funktion Omkretsen Cirkel
-{										//Skickar tillbaka omkretsen av en cirkel
-	return ((2 * PI) * r); 
+double sken_3fas(double u, double i){
+
+    double s = u * i * sqrt(3);
+
+    return s;
 }
 
-double arean_cirkel(double r)			//Funktion Arean Cirkel
-{										//Skickar tillbaka arean av en cirkel
-	return (r * r * PI);
-}
+double aktiv_3fas(double u, double i, double cos){
 
-void skriv(double t1, char *v, double t2, double r)		//Funktionen Skriv
-{// Kod från https://www.programiz.com/c-programming/examples/write-file									
-														//Skriver in uträkningarna i textfilen minir.txt
-	FILE *fp;											//(fptr = fopen("minir.txt") Öppnar filen minir.txt 		
-	fp = fopen("minir.txt", "a");						//"a" finns filen så lägger den till från slutet, så inget skrivs över
-	
-	if(fp == NULL){										//Kollar om filen hittas
-	printf("Error, , kan inte hitta filen ");			//Error om filen inte finns
-	exit(1);											//Finns inte filen avslutas funktionen	
-	}													//
-	fprintf(fp,"\n %.1f %s %.1f = %.1f",t1, v, t2, r);	//Vad som ska skrivas in i filen(Tal1, val av funktion, Tal2, Resultat)
-	fclose(fp);											//Stänger filen
-}// Kod från https://www.programiz.com/c-programming/examples/write-file
+    double p = u * i * sqrt(3) * cos;
 
-char print(char *s)										//Funktionen Print
-{	//Kod från "C från början", Av: Jan Skansholm
-														//Skriver ut det som finns i minir.txt 	
-	FILE *fil;											//
-	fil	= fopen("minir.txt", "r");						//Öppnar filen minir.txt	("r" betyder läsa (read))	
-	if (fil == NULL) {									//Kollar om filen hittas
-		printf("Error, kan inte hitta filen ");			//Error om filen inte finns
-		exit(1);										//Finns inte filen avslutas funktionen
-	}													//
-	while(fgets(s, 500, fil) != NULL)					//Skriver ut 500 tecken från filen
-	printf("%s", s);									//Skriver ut 500 tecken från filen
-	return s[500];										//Skriver ut 500 tecken från filen
-}	//Kod från "C från början", Av: Jan Skansholm
-
-
-
-int main () {
-	
-	
-	system("COLOR 2"); 		//Kod som sätter textfärgen till grön i kommandotolken.
-							//Koden är tagen från länken under
-	//http://blockofcodes.blogspot.se/2013/06/how-to-change-text-color-of-cc-console.html
-
-	double tal1 = 0;		//Variabler för programmet
-	double tal2 = 0;
-	double radien = 0;
-	double resultat = 0;
-	
-	double *pt1;			//Pointer till tal 1
-	pt1 = &tal1;
-	
-	double *pt2;			//Pointer till tal 2
-	pt2 = &tal2;
-	
-	char val[15]; 			//Den som väljer if sats, senare i programmet.
-	char las[500];			//Läs till printfunktionen (500 tecken)
-
-	while (1){				//Upprepar denna kod.
-	
-	tal1 = 0;				//Rensar värderna så man kan köra om 
-	tal2 = 0;				//programmet utan att behöva starta om det.
-	val[15] = 0;			//
-	
-	
-		printf("\nSkriv in tal 1: ");		//Skriver in tal 1
-		scanf("%lf", &tal1);				////Läser in tal 1
-		
-		printf("     ______________ Miniraknare _______________");				// Miniräknaren 
-		printf("\n    |   1.  +  plus      |   2.  -  minus      |    ");		//Skriver ut valen man har (1-10)
-		printf("\n    |   3.  *  ganger    |   4.  /  delatmed   |    ");		//
-		printf("\n    |   5.     PI        |   6.    rotenur     |	  ");		//
-		printf("\n    |   7. omkretscirkel |   8.   areancirkel  |    ");		//
-	 	printf("\n    |   9. las fil       |                     |    ");		//
-		printf("\n    |________________ 10. Exit ________________|\n\n");		//
-		
-		scanf("%s", &val);														//Skannar in valet
-		
-		if ((strcmp(val, "1") == 0) || (strcmp(val, "+") == 0) || (strcmp(val,"plus") == 0))	//If sats, plus 
-		{
-			printf("\nSkriv in tal 2: ");										//Skriver tal 2
-			scanf("%lf", &tal2);												//Läser in tal 2
-			printf("\n%.1lf + %.1lf = %.1lf \n",tal1, tal2, plus(tal1, tal2));  //Skriver ut uträkningen på skärmen
-			resultat = plus(tal1, tal2);										//
-			skriv(tal1, val, tal2, resultat);									//kör skriv funktionen	
-		}
-		
-		else if ((strcmp(val, "2") == 0) || (strcmp(val, "-") == 0) || (strcmp(val, "minus") ==0))	//If sats, minus
-		{
-			printf("\nSkriv in tal 2: ");											//Skriver tal 2
-			scanf("%lf", &tal2);													//Läser in tal 2
-			printf("\n%.1lf - %.1lf = %.1lf \n", tal1, tal2, minus(tal1, tal2));	//Skriver ut uträkningen på skärmen
-			resultat = minus(tal1, tal2);
-			skriv(tal1, val, tal2, resultat);										//kör skriv funktionen		
-		}
-		
-		else if ((strcmp(val, "3") == 0) || (strcmp(val, "*") == 0) || (strcmp(val, "ganger") ==0))	//If sats, ganger
-		{
-			printf("\nSkriv in tal 2: ");											//Skriver tal 2
-			scanf("%lf", &tal2);													//Läser in tal 2
-			printf("\n%.1lf * %.1lf = %.1lf \n",*pt1, *pt2, ganger(*pt1, *pt2));	//Skriver ut uträkningen på skärmen
-			resultat = ganger(*pt1, *pt2);
-			skriv(*pt1, val, *pt2, resultat);										//kör skriv funktionen		
-		}
-		
-		else if ((strcmp(val, "4") == 0) || (strcmp(val, "/") == 0) || (strcmp(val, "delatmed") ==0) || (strcmp(val, "delat") ==0))	//If sats, delat med
-		{
-			printf("\nSkriv in tal 2: ");											//Skriver tal 2	
-			scanf("%lf", &tal2);													//Läser in tal 2
-			printf("\n%.1lf / %.1lf = %.1lf \n",*pt1, *pt2, delat(*pt1, *pt2));		//Skriver ut uträkningen på skärmen
-			resultat = delat(*pt1, *pt2);
-			skriv(*pt1, val, *pt2, resultat);										//kör skriv funktionen		
-		}
-		
-		else if ((strcmp(val, "5") == 0) || (strcmp(val, "pi") == 0) || (strcmp(val, "PI") ==0))	//If sats, PI
-		{
-			printf("%.1lf * PI = %.1lf", tal1, t1pi(tal1));							//Skriver ut uträkningen på skärmen
-		}
-		
-		else if ((strcmp(val, "6") == 0) || (strcmp(val, "rotenur") ==0) || (strcmp(val, "roten") == 0 ))
-		{
-			printf("Roten ur %.1lf = %.1lf", tal1, rotenur(tal1));					//Skriver ut uträkningen på skärmen
-		}
-		
-		else if ((strcmp(val, "7") == 0) || (strcmp(val, "omkrets") == 0) || (strcmp(val, "omkretscirkel") ==0))	//If sats, omkrets cirkel
-		{
-			printf("\nSkriv in radien: ");
-			scanf("%lf", &radien);
-			printf("\n2 * PI * %.1lf = %.1lf \n", radien, omkrets_cirkel(radien)); 	//Skriver ut uträkningen på skärmen
-		}
-		
-		else if ((strcmp(val, "8") == 0) || (strcmp(val, "arean") == 0) || (strcmp(val, "areancirkel") ==0))		//If sats, arean cirkel
-		{
-			printf("\nSkriv in radien: ");
-			scanf("%lf", &radien);
-			printf("\n%.1lf * %.1lf * PI = %.1lf \n", radien, radien, arean_cirkel(radien));	//Skriver ut uträkningen på skärmen
-		}
-		
-		else if ((strcmp(val, "9") == 0) || (strcmp(val, "las") ==0) || (strcmp(val, "fil") ==0) || (strcmp(val, "lasfil") == 0 ))	//If sats, val
-		{
-			print(las);				//kör print funktionen	
-			printf("\n");			//ger en ny tom rad.
-		}
-		
-		else if ((strcmp(val, "10") == 0) || (strcmp(val, "stop") ==0) || (strcmp(val, "break") ==0) || (strcmp(val, "exit") ==0))  //If sats, exit 
-		{
-									//Sätter bakgrundsfärgen till 0 (svart) och skrivfärgen till 7 (vit) i kommandotolken (standard färger)
-			system("COLOR 07"); 	//Koden är tagen från länken under
-									//http://blockofcodes.blogspot.se/2013/06/how-to-change-text-color-of-cc-console.html
-
-
-
-			exit(1);				//Avslutar programmet
-		}
-		
-		else 
-		{
-			printf(" \n Error, fel inmatning \n");	//Om det blir fel vid inmatningen kommer ett Error meddelande.
-		}
-		
-	}	
-	
+    return p;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void menu_1() {
+	printf("Ohms lag spänningen(volt/V) betäckning U lika med Resistansen(Ohm) betäckning R \n");
+	printf("g�nger Str�mmen(Ampere) med betäckningen I. Kort U=R*I. \n\n");
+	double r=0, i=0;
+	printf("Skriv resistans R < 20 000ohm: \n ");
+	scanf("%lf", &r);
+
+	while(r>20000 || r==0) {
+		printf("f\x94r högt v\x84rde, f\x94rsök igen: \n");
+		scanf("%lf", &r);
+	}
+
+
+
+	printf("Skriv str�m I < 440 Ampere: \n");
+	scanf("%lf", &i);
+	while(i > 440) {
+			printf("f\x94r högt v\x84rde, f\x94rsök igen: \n");
+			scanf("%lf", &i);
+	}
+
+	printf("%f V\n", ohms_lag(r, i));
+
+
+}
+
+void menu_2() {
+		printf("Resistans sammankopplade i parallella kretsar \x84r lika med 1 delat Resistans R total \x84r lika med\n");
+		printf("Resistans 1/R1 + 1/R2 + 1/R3 d\x86 vi högst anv\x84nder tre resistanser.\n\n");
+		double r1,r2,r3;
+		printf("Skriv resistans R1 < 20 000ohm: \n");
+		scanf("%lf", &r1);
+		while(r1 > 20000)
+		{
+				printf("f\x94r högt v\x84rde, f\x94rsök igen: \n");
+				scanf("%lf", &r1);
+
+		}
+		printf("Skriv resistans R2 < 20 000ohm: \n");
+		scanf("%lf", &r2);
+		while(r2 > 20000)
+		{
+				printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+				scanf("%lf", &r2);
+
+		}
+		printf("Skriv resistans R3 < 20 000ohm: \n ");
+		scanf("%lf", &r3);
+		while(r3 > 20000)
+		{
+				printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+				scanf("%lf", &r3);
+
+		}
+		printf("%f Ohm\n", res_tot(r1, r2, r3));
+}
+
+
+void menu_3() {
+	            printf("Effektlagen enkel f\x94r likström är effekten P i Watt (W) lika med spänningen U i volt(V)\n");
+	            printf("g�nger str�mmen I i Ampere(A): \n\n");
+	            double u, i;
+	            printf("Skriv spännngen U i volt(V): \n ");
+	            scanf("%lf", &u);
+	            printf("Skriv str�m Ampere I < 440A: \n");
+	            scanf("%lf", &i);
+	            while(i > 440)
+	            {
+	                printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+ 								scanf("%lf", &i);
+	            }
+	            printf("%f W\n", eff_enk(u, i));
+}
+void menu_4() {
+
+	            printf("Skenbar effekt enfas r�knas med storheten VA(VoltAmpere) som �r lika med spänningen U i volt(V)\n");
+	            printf("g�nger str�mmen I i ampere(A)\n\n");
+	            double u, i;
+	            printf("Skriv spänningen U i volt: \n ");
+	            scanf("%lf", &u);
+	            printf("Skriv str�m I < 440A: \n");
+	            scanf("%lf", &i);
+	            while(i > 440)
+	            {
+	                printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+									scanf("%lf", &i);
+	            }
+	            printf("%f VA\n", sken_eff(u, i));
+}
+void menu_5() {
+	printf("Aktiv medelefdekt enfas �r lika med effekt P i watt(W) lika med spänningen U i volt(V) g�nger str�mmen I \n");
+	printf("i Ampere g�nger cosinus fi/efkektfaktor < 1:\n\n");
+	double u, i, cos;
+	printf("Skriv spänning U i volt: \n ");
+	scanf("%lf", &u);
+	printf("Skriv str�m I: \n");
+	scanf("%lf", &i);
+	while(i > 440){
+			printf("f\x94r högt v�rde, f\x94rsök igen:\n");
+			scanf("%lf", &i);
+	}
+	printf("Skriv in effektfaktorn cos > 0 && cos < 1:\n");
+	scanf("%lf", &cos);
+	while (cos < 0 && cos > 1)
+	{
+			printf("Fel v�rde, f\x94rsök igen\n");
+			scanf("%lf", &cos);
+	}
+	printf("%f W\n", aktiv_eff(u, i, cos));
+}
+void menu_6() {
+
+				printf("3-fas skenbar effekt �r v�xelspänning �r skenbar effekt S i voltampere(VA) lika med spänningen U i volt(V) \n");
+				printf("g�nger str�mmen I i ampere(A) g�nger roten ur 3 SQRT(3).\n\n");
+				double u, i;
+				printf("Skriv spänning U i volt(V) < 400V: \n ");
+				scanf("%lf", &u);
+				while(u > 400)
+				{
+						printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+						scanf("%lf", &u);
+				}
+
+				printf("Skriv str�m I i ampere < 440: \n");
+				scanf("%lf", &i);
+				while(i > 440)
+				{
+						printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+						scanf("%lf", &i);
+				}
+				printf("%f VA\n", sken_3fas(u, i));
+}
+void menu_7() {
+
+	            printf("3-fas aktiv effekt �r effekten P i Watt(W) lika med spänningen U i volt(V) g�nger str�mmen I i ampere(A)\n");
+	            printf("g�nger cos < 1 && cos > 0 g�nger roten ur 3 SQRT(3).\n\n");
+	            double u, i, cos;
+	            printf("Skriv spänningen U i volt(V): \n ");
+	            scanf("%lf", &u);
+	            while(u > 400)
+	            {
+	                printf("f\x94r högt v�rde, f\x94rsök igen.\n");
+	                scanf("%lf", &u);
+	            }
+	            printf("Skriv str�m I i ampere(A): \n");
+	            scanf("%lf", &i);
+	            while(i > 440)
+	            {
+	                printf("f\x94r högt v�rde, f\x94rsök igen.\n");
+	                 scanf("%lf", &i);
+	            }
+
+	            printf("Skriv in effektfaktorn cos > 0 && cos < 1: \n");
+	            scanf("%lf", &cos);
+	            while (cos < 0 && cos > 1)
+	            {
+	                printf("f\x94r högt v�rde, f\x94rsök igen: \n");
+	                scanf("%lf", &cos);
+	            }
+
+	            printf("%f W\n", aktiv_3fas(u ,i, cos));
+}
+int main()
+{
+
+	system("cls");
+	while (1)	{
+		int val;
+		val=menu_root();
+		if(val == 1)			{
+			menu_1();
+		}
+		else if(val == 2) {
+			menu_2();
+		}
+		else if(val == 3)	{
+			menu_3();
+		}
+		else if(val == 4)	{
+			menu_4();
+    }
+
+    else if(val == 5)
+    {
+      menu_5();
+    }
+    else if(val == 6)
+    {
+			menu_6();
+        }
+        else if(val == 7)
+        {
+menu_7();
+        }
+        else if (val == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            printf("Fel alternativ f\x94rsök igen!: \n");
+        }
+    }
+
+    return 0;
+}
